dd2cc2cb57b3238a98c9f342ada41f0e8a707cdf
smarien <s.marien@student.tudelft.nl>
2020-05-07 16:49:15 +0200

    first commit

---
+++ precache-manifest.6086c724130245258261d9a7ea4cc1d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f171e59e0a94d50902b58a6183b26c18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "086b87a77ae12ff9d738",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "94c305fb0e95ea5f94da",
+    "url": "/static/js/2.279b31b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.279b31b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "086b87a77ae12ff9d738",
+    "url": "/static/js/main.c75a8183.chunk.js"
+  },
+  {
+    "revision": "12bab9ae04b0287923d6",
+    "url": "/static/js/runtime-main.85a531fa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
